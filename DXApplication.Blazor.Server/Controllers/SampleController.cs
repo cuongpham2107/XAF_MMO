703942600fdf55b50a1fcb0b68ff61656d832078
thangngt-cntt <thangngt.cntt@gmail.com>
2023-02-16 21:24:08 +0700

    2023-02-16-2123

---
+++ DXApplication.Blazor.Server/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿
+using DevExpress.ExpressApp.Actions;
+using DXApplication.Module.Extension;
+
+namespace DXApplication.Blazor.Server.Controllers {
+    public class SampleController : MypeViewController {
+        public SampleController() {
+            var action1 = CreateAction(ActionType.Simple, "Copy to clipboard") as SimpleAction;
+            action1.Execute += (s, e) => {
+                //var js = JsHelper.GetJSRuntime(Application);
+                JsHelper.CopyToClipboard(Application, "Hello world");
+            };
+
+            var action2 = CreateAction(ActionType.Simple, "Go to link") as SimpleAction;
+            action2.Execute += (s, e) => {
+                var js = JsHelper.GetJSRuntime(Application);
+                JsHelper.OpenLink(Application, js, "https://vnexpress.net");
+            };
+        }
+    }
+}
